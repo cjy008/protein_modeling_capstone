b4355eac979264604edb78ae0bcc9b7456744e0c
Cindy Yu <cjy008@ucsd.edu>
2021-05-15 16:05:22 -0700

    Smaller fasta files to attempt

---
+++ tape/run_multiple_fasta_files.py
@@ -0,0 +1,28 @@
+from os import listdir
+from os.path import isfile, join, splitext
+import json
+import subprocess
+
+path = "../data/ECPred/NonEnzymes_Enzymes/"
+
+filenames = [(join(path, f), splitext(f)[0]) for f in listdir(path) if isfile(join(path, f))]
+
+for in_file, out_file in filenames:
+    with open("tape_settings.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data["tape"]["input_filename"] = in_file
+    data["tape"]["output_filename"] = out_file
+
+    with open("tape_settings.json", "w") as jsonFile:
+        json.dump(data, jsonFile, indent=4)
+
+
+    rc = call("./run_tape_batch_job.sh", shell=True)
+
+    if rc != 0:
+   		printf("Batch job script failed on {in_file}.")
+   	else:
+   		printf("Batch job script completed on {in_file}. Moving on...")
+
+printf("Completed!")
